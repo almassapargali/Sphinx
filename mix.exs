78e68899b02d767123eee9bb13771357dda37e57
Almas Sapargali <almassapargali@gmail.com>
2016-11-15 12:14:14 +0600

    add package details

--- mix.exs
+++ mix.exs
@@ -2,31 +2,38 @@ defmodule Sphinx.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :sphinx,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :sphinx,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger]]
-  end
+  def application, do: []
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [:plug: "~> 1.0"]
+  end
+
+  defp description do
+    """
+    Sphinx is a authorization library for Phoenix apps.
+    """
+  end
+
+  defp package do
+    [
+      name: :sphinx,
+      maintainers: ["Almas Sapargali"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/almassapargali/sphinx"
+      }
+    ]
   end
 end
