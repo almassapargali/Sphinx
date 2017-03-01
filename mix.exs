d7da26661b4e84e86339df9d02453673f0050766
Almas Sapargali <almassapargali@gmail.com>
2017-03-01 16:10:49 +0600

    release 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sphinx.Mixfile do
   def project do
     [
       app: :sphinx,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
